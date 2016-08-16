54db208aaea5d812ae7c57eed217ad1e7462e637
Sudhir Shakya <sudhirshakya@yahoo.com>
2016-08-16 14:12:57 +0545

    Added activity with table with static data.

---
+++ app/src/main/java/io/sorus/temples/StaticTempleTableActivity.java
@@ -0,0 +1,13 @@
+package io.sorus.temples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StaticTempleTableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_static_temple_table);
+    }
+}
