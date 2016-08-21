516550364b68c402f758e0e84c2a7df7acb6e42b
Sudhir Shakya <sudhirshakya@yahoo.com>
2016-08-21 16:59:23 +0545

    Added Google maps to project.

--- app/src/main/java/io/sorus/temples/MainActivity.java
+++ app/src/main/java/io/sorus/temples/MainActivity.java
@@ -63,4 +63,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CameraActivity.class);
         startActivity(intent);
     }
+
+    public void openMaps (View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
 }
